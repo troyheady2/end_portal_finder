f3260a7b61bdceda566fe0c56fa7497f979d9947
Troy Heady <troy.heady@pepsico.com>
2023-02-25 08:59:48 -0600

    aof6

--- epf.py
+++ epf.py
@@ -2,53 +2,53 @@
 
 import math
 from itertools import combinations
+from pprint import pprint
 
 
-def calc_slope_intercept(point,name=''):
-    x,z,theta=point
+def calc_slope_intercept(point, name=""):
+    x, z, theta = point
 
-    m=-1*math.tan(theta*math.pi / 180)
-    b=x-m*z
+    m = -1 * math.tan(theta * math.pi / 180)
+    b = x - m * z
 
-    return (m,b, name, x,z,theta)
+    return (m, b, name, x, z, theta)
 
 
-def intersect_of(p1,p2):
-    m1,b1, n1, _,_,_ = p1
-    m2,b2, n2 , _,_,_= p2
+def intersect_of(p1, p2):
+    m1, b1, n1, _, _, _ = p1
+    m2, b2, n2, _, _, _ = p2
 
-    if m1==m2:
+    if m1 == m2:
         return None
-    
-    x = (b1*m2 - b2*m1) / (m2 -m1)
+
+    x = (b1 * m2 - b2 * m1) / (m2 - m1)
     z = (b1 - b2) / (m2 - m1)
 
-    return (f'{n1} x {n2}', x, z)
+    return (f"{n1} x {n2}", x, z)
 
 
 def find_end(plist):
-    plist=[calc_slope_intercept(p,f'P{i}') for i,p in enumerate(plist)]
+    plist = [calc_slope_intercept(p, f"P{i}") for i, p in enumerate(plist)]
 
-    results=[]
-    for pair in combinations(plist,2):
+    results = []
+    for pair in combinations(plist, 2):
         results.append(intersect_of(*pair))
 
     return results
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    test = [[-200, -40, 162.3], [-1103, 0, -166.0]]
 
-    test=[ [-200, -40, 162.3],
-           [-1103,  0, -166.0 ]
-         ]
+    enigmatica6_world3 = [[5687, 919, -179.9], [4931, 908, -144.6]]
 
-    enigmatica6_world3=[ [5687, 919, -179.9],
-           [4931, 908, -144.6]
-         ]
+    AOF5_world8 = [[139.613, 32.967, 73.4], [89.282, 1333.258, 123.6]]
 
-    AOF5_world8 = [ 
-        [139.613, 32.967, 73.4],
-        [89.282, 1333.258, 123.6]
+    AOF6_kermit = [
+        # x, z, theta
+        [-208.791, 194.457, 119],
+        [-628.609, -1038.726, 77.6],
+        [-1919.7, -753.326, 119.6],
     ]
 
-    print(find_end(AOF5_world8))
+    pprint(find_end(AOF6_kermit))
